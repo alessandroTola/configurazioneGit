6df68d6f6309cdf16997325f3138d972eff6334b
alessandroTola <alessandro.tola@yahoo.it>
2021-01-29 10:14:25 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/configurazioneGit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/configurazioneGit.dir/main.cpp.obj"
+  "configurazioneGit.exe"
+  "configurazioneGit.exe.manifest"
+  "configurazioneGit.pdb"
+  "libconfigurazioneGit.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/configurazioneGit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/configurazioneGit.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for configurazioneGit.
+# This may be replaced when dependencies are built.
